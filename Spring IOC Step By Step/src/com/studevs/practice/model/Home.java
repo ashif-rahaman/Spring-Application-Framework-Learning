3d4f2c10b864f71406f94b2e1acf3d50ec6a59f8
ashif_rahaman <ashif.rahaman@hotmail.com>
2017-03-17 14:57:27 +0600

    Added model classes fields, constructors, getter, setter and Overrider toSrting method

--- Spring IOC Step By Step/src/com/studevs/practice/model/Home.java
+++ Spring IOC Step By Step/src/com/studevs/practice/model/Home.java
@@ -10,5 +10,95 @@ package com.studevs.practice.model;
  * @author ashif
  */
 public class Home {
+
+    private String name;
+    private int age;
+    private float cgpa;
+    private String address;
+
+    
+    /**
+     * Constructors with different parameters
+     */
+    
+    
+    public Home() {
+        
+    }
+
+    public Home(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Home(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Home(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Home(String name, int age, float cgpa, String address) {
+        this.name = name;
+        this.age = age;
+        this.cgpa = cgpa;
+        this.address = address;
+    }
+    
+    
+    
+    
+    /**
+     * Getters and Setters
+     * @return 
+     */
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    
+    /**
+     * toString Implemented
+     * @return 
+     */
+    
+    @Override
+    public String toString() {
+        return "Home{" + "name=" + name + ", age=" + age + ", cgpa=" + cgpa + ", address=" + address + '}';
+    }
     
 }
